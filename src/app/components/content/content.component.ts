11ef62b3ba27e86c9c4e07c93e0839aa2e07e680
Panait Andrei <panaitandrei39@gmail.com>
2022-11-28 23:32:02 +0200

    Proiect finalizat

---
+++ src/app/components/content/content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent {
+  arr = [0, 1, 2, 3, 4, 5];
+}
